5170233bace6a39bec6d821c421ef41789612fa6
GrimmOutlook <grimmoutlook87@gmail.com>
2014-11-15 09:59:05 -0500

    Initial Commit

---
+++ app.js
@@ -0,0 +1,15 @@
+angular.module('flapperNews', [])
+.controller('MainCtrl', [
+'$scope',
+function($scope){
+  $scope.test='Hello World!';
+    $scope.posts = [
+      {title: 'post 1', upvotes: 5},
+      {title: 'post 2', upvotes: 20},
+      {title: 'post 3', upvotes: 8},
+      {title: 'post 4', upvotes: 3},
+      {title: 'post 5', upvotes: 14}
+    ];
+}]);
+
+
